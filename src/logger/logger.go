5438647e24b1122a858e583028d4ed0b99f59637
aljamms8 <aljamms8@cs.man.ac.uk>
2011-10-07 18:01:46 +0100

    compact stuct def

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -13,7 +13,8 @@ func logRequest(r *http.Request) *http.Request {
 
 func main() {
 	p := &proxy.Proxy{
-		RequestManglers: []func(*http.Request)*http.Request{logRequest},
+		[]func(*http.Request)*http.Request{logRequest, logRequest},
+		[]func(*http.Response)*http.Response{},
 	}
 	err := http.ListenAndServe(":3128", p)
 	if err != nil {
